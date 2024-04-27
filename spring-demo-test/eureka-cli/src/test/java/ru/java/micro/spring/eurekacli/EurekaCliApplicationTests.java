ed3023ba36af877c9559b399307374b85cfd94c3
Nikita <ProgOutLaw@yandex.ru>
2024-04-27 12:50:32 +0300

    feat: add two modules - cli and server

---
+++ spring-demo-test/eureka-cli/src/test/java/ru/java/micro/spring/eurekacli/EurekaCliApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.java.micro.spring.eurekacli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-demo-test/eureka-server/src/test/java/ru/java/micro/spring/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.java.micro.spring.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
