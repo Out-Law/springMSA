07f5255635a9812abd82bc00d8ee9484322de528
Nikita <ProgOutLaw@yandex.ru>
2024-10-22 15:25:44 +0300

    docker

--- spring-demo-test/cli-two/src/main/java/ru/java/micro/spring/clitwo/сonsumer/KafkaConsumer.java
+++ spring-demo-test/cli-two/src/main/java/ru/java/micro/spring/clitwo/сonsumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package ru.java.micro.spring.clitwo.сonsumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -8,32 +9,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumer {
 
-//    @KafkaListener(topics = "test_topic", groupId = "cli-two-group")
-//    public void listen(String message) {
-//        System.out.println("Received message: " + message);
-//    }
-
-    private static final String TOPIC_OUT = "serviceB_to_serviceC";
-    private static final String TOPIC_COMPENSATE = "compensate_serviceA";
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
-
-    @KafkaListener(topics = "serviceA_to_serviceB", groupId = "service-b-group")
-    public void consume(String message) {
-        try {
-            // Выполнение промежуточной логики
-            // Если успех, отправляем сообщение следующему микросервису
-            kafkaTemplate.send(TOPIC_OUT, message);
-        } catch (Exception e) {
-            // Если неудача, отправляем сообщение для отката
-            kafkaTemplate.send(TOPIC_COMPENSATE, message);
-        }
+    @KafkaListener(topics = "test-topic", groupId = "test-group")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
     }
 
-    @KafkaListener(topics = "compensate_serviceB", groupId = "service-b-group")
-    public void compensate(String message) {
-        System.out.println("compensate_serviceB: Откат, передача информации A");
-        kafkaTemplate.send(TOPIC_COMPENSATE, message);
+    String bd = "";
+    @KafkaListener(topics = "orders", groupId = "test-group")
+    public void processOrder(ConsumerRecord<String, String> record) {
+        String orderId = record.key();
+        String orderDetails = record.value();
+        System.out.println("Processing order: OrderId=" + orderId + ", Details=" + orderDetails);
+        bd = "Processing order: OrderId=" + orderId + ", Details=" + orderDetails;
+        // Логика обработки заказа, например, сохранение в базу данных инфрмацию о заказе
     }
+
 }
